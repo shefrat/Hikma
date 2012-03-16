58aec4dea0e1c4e7d7668bb7796143156ef742b3
HMSoft <hichemraz@gmail.com>
2012-03-16 08:27:08 +0300

    Install Hikma

---
+++ install.sh
@@ -0,0 +1,7 @@
+#/usr/bin/bash
+
+sudo cp icons/hikma.png /usr/share/icons
+sudo mkdir /opt/hikma
+sudo cp -R * /opt/hikma
+sudo cp "applications/إعدادات حكمة.desktop" /usr/share/applications
+sudo cp applications/hikma.desktop /usr/share/applications
